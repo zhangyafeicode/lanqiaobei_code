51a245842262d62660bb89fb0503419736703ff7
zhangyafeicode <zhangyafei5427@gmail.com>
2025-05-09 20:23:51 +0800

    独立按键控制LED灯的状态

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
+
 #include <REGX52.H>
+
+
+void Delay(unsigned int xms)	//@12.000MHz
+{
+	while(xms)
+	{
+		unsigned char data i, j;
+
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+		xms--;
+	}
+}
+
 
 void main()
 {
-//	P2 = 0xFE;
-	P2_0 = 0;
-	
+	while(P3_1 == 1)
+	{
+		P2_0 == 1;
+		
+	}
 	while(1)
 	{
-		if(P3_1 == 0)
+		if(P3_1 == 0);
 		{
-			P2_0 = 0;
-		}
-		else
-		{
-			P2_0 = 1;
+			Delay(20);
+			while(P3_1 == 0);
+			Delay(20);
+			
+			P2_0 = ~P2_0;
+			
 		}
 	}
 }
